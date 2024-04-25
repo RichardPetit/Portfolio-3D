5872ae4a83f03a77c3ef08f19afffc6a4f265df3
Richard PETIT <r.petit@human-immobilier.fr>
2024-04-25 11:37:39 +0200

    Mise en place du ciel, oiseau et avion apparaissent + début création système de rotation.

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -13,9 +13,48 @@ import { a } from '@react-spring/three';
 
 import islandScene from '../assets/3d/island.glb';
 
-const Island = (props) =>{
+const Island = ({isRotating, setIsRotating, ... props}) =>{
   const islandRef = useRef();
+  
+  const { gl, viewport } = useThree();
   const { nodes, materials } = useGLTF(islandScene);
+
+  const lastX = useRef(0);
+  const rotationSpeed = useRef(0);
+  const dampingFactor = 0.95;
+
+  const handlePointerDown = (e) =>{
+    e.stopProgation();
+    e.preventDefault();
+    setIsRotating(true);
+
+    const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+    lastX.current = clientX;
+  }
+  const handlePointerUp = (e) =>{
+    e.stopProgation();
+    e.preventDefault();
+    setIsRotating(false);
+
+    const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+    const delta = (clientX - lastX.current) / viewport.width;
+
+    islandRef.current.rotation.y += delta * 0.01 * Math.PI;
+    lastX.current = clientX;
+    rotationSpeed.current = delta * 0.01 * Math.PI;
+
+  }
+  const handlePointerMove = (e) =>{
+    e.stopProgation();
+    e.preventDefault();
+    if(isRotating){
+      handlePointerUp(e);
+    }
+  }
+  
+  useEffect(() => {
+
+  }, [gl, handlePointerDown, handlePointerUp, handlePointerMove]);
   return (
     <a.group ref= {islandRef} {...props}>
       <mesh
