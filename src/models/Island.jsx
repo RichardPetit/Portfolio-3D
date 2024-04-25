148efd2d74be90b69002e3add2aa27884781353a
Richard PETIT <r.petit@human-immobilier.fr>
2024-04-25 10:12:05 +0200

    Modif et ajout d'éléments pour obtenir un premier affichage de l'ile.

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -9,62 +9,48 @@ Title: Fox's islands
 import { useRef, useEffect } from 'react';
 import { useGLTF } from '@react-three/drei';
 import { useFrame, useThree } from '@react-three/fiber';
+import { a } from '@react-spring/three';
 
 import islandScene from '../assets/3d/island.glb';
 
 const Island = (props) =>{
-  const { nodes, materials } = useGLTF('/island.glb')
+  const islandRef = useRef();
+  const { nodes, materials } = useGLTF(islandScene);
   return (
-    <group {...props} dispose={null}>
+    <a.group ref= {islandRef} {...props}>
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.polySurface944_tree_body_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.polySurface945_tree1_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.polySurface946_tree2_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        castShadow
-        receiveShadow
+        
+        
         geometry={nodes.polySurface947_tree1_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.polySurface948_tree_body_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.polySurface949_tree_body_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.pCube11_rocks1_0.geometry}
         material={materials.PaletteMaterial001}
       />
-    </group>
+    </a.group>
   )
 }
 
-useGLTF.preload('/island.glb')
-
 export default Island;
-
-
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,29 @@
-import Suspense from 'react'
+import { Suspense } from 'react';
+
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
+import Island from '../models/Island';
     {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
         POPUP
         </div> */}
 
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
+
+    if(window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [ screenScale, screenPosition, rotation]
+  }
+
+  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+
   return (
     
     <section className='w-full h-screen relative'>
@@ -22,7 +40,11 @@ const Home = () => {
 
 
 
-
+          <Island 
+          position={islandPosition}
+          scale = {islandScale}
+          rotation = {islandRotation}
+          />
         </Suspense>
       </Canvas>
 
