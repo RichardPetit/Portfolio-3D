4471c2a3d6c273b76851b6aa7881bb5c690e90f2
Richard PETIT <r.petit@human-immobilier.fr>
2024-05-10 14:45:45 +0200

    Début de la mise en place de la rotation de l'ile.

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -24,35 +24,96 @@ const Island = ({isRotating, setIsRotating, ... props}) =>{
   const dampingFactor = 0.95;
 
   const handlePointerDown = (e) =>{
-    e.stopProgation();
+    e.stopPropagation();
     e.preventDefault();
     setIsRotating(true);
 
     const clientX = e.touches ? e.touches[0].clientX : e.clientX;
     lastX.current = clientX;
   }
+
   const handlePointerUp = (e) =>{
-    e.stopProgation();
+    e.stopPropagation();
     e.preventDefault();
     setIsRotating(false);
+  }
 
-    const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+  const handlePointerMove = (e) =>{
+    e.stopPropagation();
+    e.preventDefault();
+    if(isRotating){
+      const clientX = e.touches ? e.touches[0].clientX : e.clientX;
     const delta = (clientX - lastX.current) / viewport.width;
 
     islandRef.current.rotation.y += delta * 0.01 * Math.PI;
     lastX.current = clientX;
-    rotationSpeed.current = delta * 0.01 * Math.PI;
-
-  }
-  const handlePointerMove = (e) =>{
-    e.stopProgation();
-    e.preventDefault();
-    if(isRotating){
-      handlePointerUp(e);
+    rotationSpeed.current = delta * 0.01 * Math.PI;;
     }
   }
-  
+
+  const handelKeyDown = (e) => {
+    if(e.key === 'ArrowLeft') {
+      if(!isRotating) setIsRotating(true);
+      islandRef.current.rotation.y += 0.01 * Math.PI;
+    } else if (e.key === 'ArrowRight') {
+      if(!isRotating) setIsRotating(true);
+      islandRef.current.rotation.y -= 0.01 * Math.PI;
+    }
+  }
+
+  const handelKeyUp = (e) => {
+    if(e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+      setIsRotating(false);  
+    }
+  }
+
+  useFrame(() => {
+    if(!isRotating) {
+      rotationSpeed.current *= dampingFactor;
+      if(Math.abs(rotationSpeed.current) < 0.001){
+        rotationSpeed.current = 0;
+      } else {
+        const rotation = islandRef.current.rotation.y;
+
+        const normalizedRotation =
+        ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI);
+
+      // Set the current stage based on the island's orientation
+        switch (true) {
+          case normalizedRotation >= 5.45 && normalizedRotation <= 5.85:
+            setCurrentStage(4);
+            break;
+          case normalizedRotation >= 0.85 && normalizedRotation <= 1.3:
+            setCurrentStage(3);
+            break;
+          case normalizedRotation >= 2.4 && normalizedRotation <= 2.6:
+            setCurrentStage(2);
+            break;
+          case normalizedRotation >= 4.25 && normalizedRotation <= 4.75:
+            setCurrentStage(1);
+            break;
+          default:
+            setCurrentStage(null);
+        }
+      }
+    }
+  })
+
   useEffect(() => {
+    const canvas = gl.domElement;
+    canvas.addEventListener('pointerdown', handlePointerDown);
+    canvas.addEventListener('pointerup', handlePointerUp);
+    canvas.addEventListener('pointermove', handlePointerMove);
+    document.addEventListener('keydown', handelKeyDown);
+    document.addEventListener('keyup', handelKeyUp);
+
+    return () => {
+      canvas.removeEventListener('pointerdown', handlePointerDown);
+      canvas.removeEventListener('pointerup', handlePointerUp);
+      canvas.removeEventListener('pointermove', handlePointerMove);
+      document.removeEventListener('keydown', handelKeyDown);
+      document.removeEventListener('keyup', handelKeyUp);
+    }
 
   }, [gl, handlePointerDown, handlePointerUp, handlePointerMove]);
   return (
